5998b393ed78dbda652397cc4e324500ff27a1e2
Dave Sieh <dj0hnve@gmail.com>
2013-12-15 13:28:02 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+VCNL4000 Proximity Sensor Arduino Library
+=========================================
+
+This project contains the definition of an Arduino library for the VCNL4000
+sensor. This sensor is able to perform relatively short (< 7 inches) proximity
+sensing and light intensity readings.
+
+The commands for the sensor are such that you can read proximity readings
+independenty of ambient light levels.
+
---
+++ src/sketch.ino
@@ -0,0 +1,42 @@
+// -*- c -*-
+// Example sketch for talking to the VCNL4000 i2c proximity/light sensor
+// Written by Adafruit! Public domain.
+// To use: Connect VCC to 3.3-5V (5V is best if it is available), GND to
+//         ground, SCL to i2c clock (on classic arduinos, Analog 5), SDA
+//         to i2c data (on classic arduinos Analog 4). The 3.3v pin is
+//         an ouptut if you need 3.3V
+// This sensor is 5V compliant so you can use it with 3.3 or 5V micros
+
+#include <VCNL4000.h>
+
+VCNL4000 proximitySensor;
+boolean state = false;
+
+void setup() {
+  Serial.begin(9600);
+
+  proximitySensor.begin();
+}
+
+void loop() {
+  uint16_t distance = proximitySensor.readProximity();
+  /* uint16_t ambient = proximitySensor.readAmbient(); */
+
+  /* Serial.print("Distance to target: "); */
+  /* Serial.println(distance); */
+  if (distance > 2150) {
+    if (!state) {
+      Serial.println("Turn sound on");
+      state = true;
+    }
+  } else {
+    if (state) {
+      Serial.println("Turn sound off");
+      state = false;
+    }
+  }
+  /* Serial.print("Ambient light level: "); */
+  /* Serial.println(ambient); */
+
+  delay(100);
+}
